7c6ed4186bef9024af138076b0dd9467a83f4da4
iAyubAnsari <emailforayub@gmail.com>
2016-11-28 17:45:09 +0530

    BackButton top of the corner

--- app/src/main/java/com/example/ayub/miwok/Word.java
+++ app/src/main/java/com/example/ayub/miwok/Word.java
@@ -4,5 +4,62 @@ package com.example.ayub.miwok;
  * Created by Ayub on 28-Nov-16.
  */
 
+
 public class Word {
+
+
+    private String mDefaultTranslation;
+
+
+    private String mMiwokTranslation;
+
+
+    private int mAudioResourceId;
+
+
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioResourceId;
+    }
+
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId,
+                int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
+    }
+
+
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+
+    public int getAudioResourceId() {
+        return mAudioResourceId;
+    }
 }
--- app/src/main/java/com/example/ayub/miwok/WordAdapter.java
+++ app/src/main/java/com/example/ayub/miwok/WordAdapter.java
@@ -1,8 +1,59 @@
 package com.example.ayub.miwok;
 
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 /**
  * Created by Ayub on 28-Nov-16.
  */
+public class WordAdapter extends ArrayAdapter<Word> {
+    private int mColorResourceId;
 
-public class WordAdapter {
+
+    public WordAdapter(Context context, ArrayList<Word> words, int colorResourceId){
+        super(context,0,words);
+        mColorResourceId=colorResourceId;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView=convertView;
+        if(listItemView==null){
+            listItemView= LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item,parent,false);
+        }
+
+        Word currentWord=getItem(position);
+
+        TextView miwokTextView=(TextView) listItemView.findViewById(R.id.miwok_text_view);
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        TextView defaultTextView=(TextView) listItemView.findViewById(R.id.default_text_view);
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        ImageView imageView=(ImageView) listItemView.findViewById(R.id.images);
+        if (currentWord.hasImage()) {
+            // If an image is available, display the provided image based on the resource ID
+            imageView.setImageResource(currentWord.getImageResourceId());
+            // Make sure the view is visible
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            // Otherwise hide the ImageView (set visibility to GONE)
+            imageView.setVisibility(View.GONE);
+        }
+        View textContainer=listItemView.findViewById(R.id.text_container);
+        int color= ContextCompat.getColor(getContext(),mColorResourceId);
+        textContainer.setBackgroundColor(color);
+
+        return listItemView;
+
+    }
 }
